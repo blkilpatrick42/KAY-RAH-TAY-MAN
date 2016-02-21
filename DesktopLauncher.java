759f20d09440b2324e6bce25187c2a3dd79babe4
blkilpatrick42 <blkilpatrick42@gmail.com>
2016-02-20 18:58:48 -0600

    Alpha 1 changes to DesktopLauncher
    The changes here involve adding the parameters config.width and config.height for modifying the window size, and adding a few comments for clarity

--- DesktopLauncher.java
+++ DesktopLauncher.java
@@ -7,6 +7,11 @@ import com.gdx.game.KayRahTayMan;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new KayRahTayMan(), config);
+                
+                config.width = 640;     //horizontal width of the app window
+                config.height = 480;    //vertical height of the app window
+                
+                //Creates and runs an instance of the kayratayman game
+		new LwjglApplication(new KayRahTayMan(), config);  
 	}
 }
